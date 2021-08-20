1c3d8793e8f2b50e89a4d54550d017e6b4b6aee8
wardde <46535794+wardde@users.noreply.github.com>
2021-08-20 18:50:55 -0500

    Added pick round and number + nicer formatting

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 'use strict'
 
+var pickNum = 1;
+
+const teams = 3;
 const draftPlayer = document.querySelector('.draftPlayer');
 
 draftPlayer.addEventListener('click', function() {
     let playerName = player.value;
     
-    var li = document.createElement('li');
-    li.className = 'draftedPlayer';
-    var textNode = document.createTextNode(playerName);
-    li.appendChild(textNode);
-    document.getElementById('draftList').appendChild(li);
+    var pickedPlayer = document.createElement('h3');
+    pickedPlayer.className = 'pickedPlayer';
+
+    let round = 1 + Math.floor((pickNum - 1)/teams);
+    let roundPick = ((pickNum - 1) % teams) + 1;
+    let pickStr = round + '.' + (roundPick < 10 ? '0' : '') + roundPick;
+
+    var textNode = document.createTextNode(pickStr + ' ' + playerName + '\n');
+    pickedPlayer.appendChild(textNode);
+    document.body.appendChild(pickedPlayer);
 
     player.value = '';
+    ++pickNum;
     
     console.log('Draft ' + playerName);
-})
+});
